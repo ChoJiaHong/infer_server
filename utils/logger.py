13058eb6ff58b66a0a5d70dcf799870bb8d6c028
ChoJiaHong <63405306+ChoJiaHong@users.noreply.github.com>
2025-07-01 06:55:10 +0800

    log start and end timestamps

--- utils/logger.py
+++ utils/logger.py
@@ -20,7 +20,7 @@ FIELDNAMES = [
     "request_id", "client_ip", "batch_size",
     "wait_ms", "trigger_type", "trigger_time_ms",
     "inference_ms", "preprocess_ms", "postprocess_ms", "total_ms",
-    "receive_ts", "batch_id"
+    "receive_ts", "batch_id", "start_ts", "end_ts"
 ]
 
 class RequestLogger:
@@ -37,6 +37,8 @@ class RequestLogger:
             "total_ms": 0,
             "receive_ts": "",
             "batch_id": None,
+            "start_ts": time.strftime("%Y-%m-%d %H:%M:%S"),
+            "end_ts": "",
         }
         self.time_ref = {}
         self._start_total = time.time()
@@ -75,6 +77,7 @@ class RequestLogger:
 
     def write(self):
         self.fields["total_ms"] = (time.time() - self._start_total) * 1000
+        self.fields["end_ts"] = time.strftime("%Y-%m-%d %H:%M:%S")
 
         is_new = not os.path.exists(LOG_PATH)
         with open(LOG_PATH, "a", newline="") as f:
